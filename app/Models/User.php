f57b4ef4422100f467e94e37d3ac147a82a9ec8a
Gilang Ramadhani Al <gilang.alifianto@gmail.com>
2024-12-16 00:49:16 +0700

    Perbaikan login
    Terdapat perbaikan login, login sekarang menggunakan laravel breeze

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,11 +15,10 @@ class User extends Authenticatable
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'name',
-        'username',
         'email',
         'password',
         'role',
@@ -28,7 +27,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $hidden = [
         'password',
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\RoleMiddleware;
-use App\Http\Controllers\UserController;
 
-Route::get('/register', function () {
-    return view('welcome');
-});
-// Rute admin
+Route::view('/', 'welcome');
 
-Route::middleware(['role:admin'])->group(function () {
-    Route::get('/admin/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-});
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified', 'user'])
+    ->name('dashboard');
 
-Route::middleware(['role:user'])->group(function () {
-    Route::get('/user/dashboard', [UserDashboardController::class, 'index'])->name('user.dashboard');
-});
+    Route::view('admin/dashboard', 'admin.dashboard')
+    ->middleware(['auth', 'verified', 'admin'])
+    ->name('admin.dashboard');
 
 
-Route::get('/register', [UserController::class, 'showRegisterForm'])->name('register');
-Route::post('/register',[UserController::class, 'submitRegisterForm'])->name('register.submit');
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
-Route::get('/login', [UserController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [UserController::class, 'submitLoginForm'])->name('login.submit');
+require __DIR__.'/auth.php';
