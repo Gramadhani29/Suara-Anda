a21fcba0411a3a10ef79adc79eda278afd0f1b25
Gilang Ramadhani Al <gilang.alifianto@gmail.com>
2024-12-29 18:15:14 +0700

    Update sidebar.blade.php

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -53,6 +53,7 @@
         </a>
       </li>
     </ul>
+    
     @if( Auth::user()->roles == 'ADMIN')
 
     <ul>
@@ -105,7 +106,7 @@
   @keydown.escape="closeSideMenu">
   <div class="py-4 text-gray-500 dark:text-gray-400">
     <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
-      APLIKASIKU
+      Suara Anda
     </a>
     <ul class="mt-6">
       <li class="relative px-6 py-3">
